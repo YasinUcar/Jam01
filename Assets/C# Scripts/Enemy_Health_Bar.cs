792fc39ea47aa37e61067ef7be406ab9426f74f8
Ali-Durman <99588223+Ali-Durman@users.noreply.github.com>
2022-07-03 22:32:32 +0300

    ..
    ..

--- Assets/C# Scripts/Enemy_Health_Bar.cs
+++ Assets/C# Scripts/Enemy_Health_Bar.cs
@@ -33,8 +33,8 @@ public class Enemy_Health_Bar : MonoBehaviour
 
         if (currenthealth <= 0)
         {
-            hitParticle.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-            hitParticle.GetComponent<ParticleSystem>().Play();
+           // hitParticle.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            // hitParticle.GetComponent<ParticleSystem>().Play();
             Destroy(goblin.gameObject);
 
         }
